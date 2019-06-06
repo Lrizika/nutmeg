56106eb21a2dfd16e16fa3d33626a2655b9c6583
larkeith <llarkeith@gmail.com>
2019-06-05 17:38:39 -0700

    Added user input, outgoing message support Created Message object (uses curses pads) Moved queueing to MessageDisplay Now able to display multiline messages properly Started storing y, x for each window, for convenience Created getLastChar util function Fixed cursor positioning (I hope) Created InputBox for user input, only one line max for now Enabled scrolling up and down through message history Still need to add support for getting to the top of the stored history Created Controller for managing the following: - User input and special keystrokes - Offset - Room management - In the future:     - Commands     - Login, logout, etc     - ???     - Profit
    Next priorities:
    Enable room switching
    Allow for lazy loading prior messages
    Set up other room displays
    Support commands (mainly room switching)
    Build login flow
    Allow sending multiline messages
    Improve docstrings
    (Eventually) remove crash on resize

--- python/client.py
+++ python/client.py
@@ -3,6 +3,7 @@
 from matrix_client.client import MatrixClient
 import datetime
 import curses
+from curses import textpad
 
 import logging
 
@@ -22,7 +23,7 @@ def tsToDt(timestamp: str) -> str:
 		str -- Y-M-D or H:M:S
 	"""
 	dt = datetime.datetime.fromtimestamp(int(timestamp)/1000)
-	if abs(datetime.datetime.today()-dt) < datetime.timedelta(1):
+	if abs(datetime.datetime.today()-dt) < datetime.timedelta(days=1):
 		return(dt.strftime('%H:%M:%S'))
 	return(dt.strftime('%Y-%m-%d'))
 	#return(dt.strftime('%Y-%m-%d %H:%M:%S'))
@@ -45,6 +46,25 @@ def getUser(room, userId):
 			'roomName': str(room.display_name),
 			'roomId': str(room.room_id)})
 
+def getLastChar(window) -> tuple:
+	"""
+	Returns the last filled character in a window object.
+	
+	Arguments:
+		window {curses.window} -- The window to check
+	
+	Returns:
+		tuple (y,x) -- The (y,x) of the last filled character, relative to the window
+	"""
+
+	height, width = window.getmaxyx()
+	for y in range(height-1, -1, -1):
+		for x in range(width-1, -1, -1):
+			if window.instr(y,x,1) != b' ':
+				return(y,x)
+	raise IndexError('No filled characters in window.')
+
+
 
 class OutgoingText:
 	"""
@@ -54,36 +74,14 @@ class OutgoingText:
 	"""
 
 	def __init__(self, text, room, completeHandler, backfill: int=0):
-		self.room = room
-		self.text = text
-		self.eventId = self.send()['event_id']
+		self.eventId = room.send_text(text)['event_id']
 		if backfill: room.backfill_previous_messages(limit=backfill)
 		completeHandler(room, getEvent(room, self.eventId))
-	def send(self):
-		return(self.room.send_text(self.text))
 
-class MessageQueue:
-	def __init__(self, rooms = None):
-		if rooms is None: rooms = []
-		self.messages = {}
-		for room in rooms: self.messages[room] = []
-
-	def addRoom(self, room):
-		if room not in self.messages:
-			self.messages[room] = []
-	
-	def put(self, room, message: str):
-		self.messages[room].append(message)
-
-	def get(self, room, count: int=10, reverse: bool=False):
-		ret = self.messages[room][-count:]
-		if reverse is True: ret.reverse()
-		return(ret)
 
 class EventManager:
 	"""
 	Handles incoming events.
-	Usage: eventManager.handleEvent(eventId)
 	"""
 
 	def __init__(self, displayManager):
@@ -91,6 +89,7 @@ class EventManager:
 		self.handled = []
 
 	def handleEvent(self, room, event):
+		oldY, oldX = curses.getsyx()
 		if event['event_id'] not in self.handled:
 			logging.info('Handling event: '+event['event_id'])
 			if event['type'] == 'm.room.message': 
@@ -101,35 +100,57 @@ class EventManager:
 			self.handled.append(event['event_id'])
 		else:
 			logging.info('Already handled event: '+event['event_id'])
+		self.displayManager.screen.move(oldY, oldX)
+		self.displayManager.screen.refresh()
 
 	def handleMessage(self, room, event):
 		self.displayManager.messageDisplay.printMessage(room, event)
 		
 class DisplayManager:
-	def __init__(self, screen, startRoom = None):
+	def __init__(self, screen, y, x, startRoom = None):
 		self.screen = screen
+		self.y = y
+		self.x = x
 		self.height, self.width = screen.getmaxyx()
 		self.currentRoom = None
 
 		# TODO: Change new windows into new pads
-		self.messageQueue = MessageQueue()
-		self.statusScreen = curses.newwin(1,self.width,0,0)
-		self.statusDisplay = StatusDisplay(self.statusScreen)
-		self.messageScreen = curses.newwin(self.height-2,self.width,1,0)
-		self.messageDisplay = MessageDisplay(self.messageScreen, self.messageQueue)
+		#self.messageQueue = MessageQueue()
+		statusY = 0
+		statusX = 0
+		self.statusScreen = curses.newwin(1,self.width,statusY,statusX)
+		self.statusDisplay = StatusDisplay(self.statusScreen, statusY, statusX)
+		messageY = 1
+		messageX = 10
+		self.messageScreen = curses.newwin(self.height-messageY-1,self.width-messageX, messageY, messageX)
+		self.messageDisplay = MessageDisplay(self.messageScreen, messageY, messageX)#, self.messageQueue)
+		inputY = self.height-1
+		inputX = 0
+		self.inputScreen = curses.newwin(self.height-inputY,self.width-inputX, inputY, inputX)
+		self.inputBox = InputBox(self.inputScreen, inputY, inputX)
 		if startRoom is not None: self.changeRoom(startRoom)
 		
 	def changeRoom(self, room):
 		self.currentRoom = room
-		self.messageQueue.addRoom(room)
+		#self.messageQueue.addRoom(room)
 		self.currentRoom.update_room_name()
 		self.currentRoom.update_room_topic()
 		self.currentRoom.update_aliases()
 		self.statusDisplay.printRoomHeader(room)
 
-class StatusDisplay:
-	def __init__(self, screen):
+class InputBox:
+	def __init__(self, screen, y, x):
 		self.screen = screen
+		self.y = y
+		self.x = x
+		self.height, self.width = screen.getmaxyx()
+		self.textbox = textpad.Textbox(self.screen, insert_mode=True)
+
+class StatusDisplay:
+	def __init__(self, screen, y, x):
+		self.screen = screen
+		self.y = y
+		self.x = x
 		self.height, self.width = screen.getmaxyx()
 
 	def printStatus(self, status):
@@ -163,10 +184,14 @@ class StatusDisplay:
 		self.printStatus(status)
 
 class MessageDisplay:
-	def __init__(self, screen, messageQueue):
+	def __init__(self, screen, y, x):#, messageQueue):
 		self.screen = screen
+		self.y = y
+		self.x = x
+		self.offset = 0
 		self.height, self.width = screen.getmaxyx()
-		self.messageQueue = messageQueue
+		#self.messageQueue = messageQueue
+		self.messageQueue = {}
 		
 	def printMessage(self, room, event):
 		self.queueMessage(room, event)
@@ -174,35 +199,107 @@ class MessageDisplay:
 
 	def printQueue(self, room):
 		self.screen.clear()
-		y=0
-		for message in self.messageQueue.get(room, count=self.height):
-			self.screen.addstr(y, 0, message)
-			y+=1
 		self.screen.refresh()
-
+		y=self.height + self.y
+		queue = list(reversed(self.messageQueue[room]))[self.offset:self.height+self.offset]
+		# TODO: Load more messages if queue is too short
+		for message in queue:#.get(room, count=self.height):
+			y -= message.printHeight
+			if y+message.printHeight<=self.y: break
+			message.pad.refresh(max(0, self.y-y),0, max(y, self.y),self.x, max(y+message.printHeight-1, self.y),self.width+self.x)
+			#logging.info((max(0, self.y-y),0, max(y, self.y),self.x, max(y+message.printHeight-1, self.y),self.width+self.x))
+			
 	def queueMessage(self, room, event):
-		if event['content']['msgtype'] == 'm.text':
-			message = ('%(timestamp)s - %(sender)s: %(message)s' %
-				{'timestamp': tsToDt(str(event['origin_server_ts'])),
-				'sender': str(getUser(room, event['sender']).get_display_name()),
-				'message': str(event['content']['body'])})
-			self.messageQueue.put(room, message)
-			#self.screen.clear()
-			#self.screen.addstr(0,0, message)
-			#self.screen.refresh()
-			#logging.info(message)
-		else:
-			raise TypeError('Unknown event content msgtype: %(msgtype)s' %
-				{'msgtype': str(event['content']['msgtype'])})
+		if room not in self.messageQueue: self.messageQueue[room] = []
+		
+		messagePad = curses.newpad(self.height, self.width)
+		self.messageQueue[room].append(Message(messagePad, room, event))
 		#else:
 		#	raise NotImplementedError('Handling for messages in other rooms is not yet implemented.')
 
+class Message:
+	def __init__(self, pad, room, event):
+		self.event = event
+		self.room = room
+		self.pad = pad
+		self.height, self.width = pad.getmaxyx()
+		self.build()
+		try:
+			self.printHeight = getLastChar(self.pad)[0]+1
+		except IndexError:
+			self.printHeight = 0
+
+	def build(self):
+		if self.event['content']['msgtype'] == 'm.text':
+			self.buildText()
+		else:
+			logging.warning('Unknown event content msgtype "%(msgtype)s" while building message for event: %(event)s' %
+				{'msgtype': str(self.event['content']['msgtype']),
+				'event': str(self.event)})
+			self.buildBroken()
+
+	def buildText(self):
+		message = ('%(timestamp)s - %(sender)s: %(message)s' %
+			{'timestamp': tsToDt(str(self.event['origin_server_ts'])),
+			'sender': str(getUser(self.room, self.event['sender']).get_display_name()),
+			'message': str(self.event['content']['body'])})
+		self.pad.addstr(0,0,message)
+		logging.info('Built message: '+message)
+
+	def buildBroken(self):
+		message = ('%(timestamp)s: Something went wrong displaying message %(eventId)s from %(sender)s. Check the logs for more info.' %
+			{'timestamp': tsToDt(str(self.event['origin_server_ts'])),
+			'sender': str(getUser(self.room, self.event['sender']).get_display_name()),
+			'eventId': str(self.event['event_id'])})
+		self.pad.addstr(0,0,message)
+		logging.info('Built message: '+message)
+		
+class Controller:
+	def __init__(self, client, eventManager):
+		self.client = client
+		self.currentRoom = None
+		self.offset = 0
+		self.rooms = []
+		self.eventManager = eventManager
+
+	def joinRoom(self, roomId):
+		room = self.client.join_room(roomId)
+		self.currentRoom = room
+		if room not in self.rooms:
+			room.add_listener(self.eventManager.handleEvent)
+			room.backfill_previous_messages(limit=self.eventManager.displayManager.messageDisplay.height+100)
+		self.rooms.append(room)
+		self.eventManager.displayManager.changeRoom(room)
+		self.client.stop_listener_thread()
+		self.client.start_listener_thread()
+
+	def changeOffset(self, amount):
+		self.offset += amount
+		if self.offset < 0: self.offset = 0
+		self.eventManager.displayManager.messageDisplay.offset = self.offset
+		self.eventManager.displayManager.messageDisplay.printQueue(self.currentRoom)
+
+	def setOffset(self, num):
+		self.offset = num
+		self.eventManager.displayManager.messageDisplay.offset = self.offset
+		self.eventManager.displayManager.messageDisplay.printQueue(self.currentRoom)
+
+	def inputListener(self, keystroke):
+		if keystroke == curses.KEY_UP:
+			self.changeOffset(1)
+		elif keystroke == curses.KEY_DOWN:
+			self.changeOffset(-1)
+		elif keystroke == curses.KEY_PPAGE:
+			self.changeOffset(10)
+		elif keystroke == curses.KEY_NPAGE:
+			self.changeOffset(-10)
+		return(keystroke)
 
 
 logging.basicConfig(filename='nutmeg.log',level=logging.INFO)
 
 def main(stdscr):
-	displayManager = DisplayManager(stdscr)
+	displayManager = DisplayManager(stdscr, 0, 0)
 	# Clear screen
 	stdscr.clear()
 	displayManager.statusDisplay.printStatus('Loading Nutmeg...')
@@ -225,27 +322,20 @@ def main(stdscr):
 	token = client.login_with_password(username=USERNAME, password=PASSWORD)
 
 	displayManager.statusDisplay.printJoining(ROOMNAME, HOMESERVER)
-	room = client.join_room('#%(roomName)s:%(homeServer)s' % 
+
+	controller = Controller(client, EventManager(displayManager))
+	
+	controller.joinRoom('#%(roomName)s:%(homeServer)s' % 
 		{'roomName': ROOMNAME,
 		'homeServer': HOMESERVER})
-	#room.add_listener(print)
-	displayManager.changeRoom(room)
-	#displayManager = DisplayManager(stdscr, startRoom=room)
-	eventManager = EventManager(displayManager)
-	room.add_listener(eventManager.handleEvent)
-	room.backfill_previous_messages(limit=displayManager.messageDisplay.height+2)
-
-	try:
-		OutgoingText('Hallo!', room, eventManager.handleEvent, backfill=3)
-	except:
-		logging.error(room.events)
-		raise
-
-	client.start_listener_thread()
 
 	while True:
-		stdscr.refresh()
-		stdscr.getkey()
+		out = displayManager.inputBox.textbox.edit(controller.inputListener)
+		controller.setOffset(0)
+		displayManager.inputBox.screen.clear()
+		displayManager.inputBox.screen.refresh()
+		if out:
+			OutgoingText(out, controller.currentRoom, controller.eventManager.handleEvent, backfill=3)
 
 curses.wrapper(main)
 
